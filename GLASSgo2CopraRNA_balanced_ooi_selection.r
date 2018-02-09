7db90f83a1ec9a784d7a9d478ced175cf56f8d22
JensGeorg <33146097+JensGeorg@users.noreply.github.com>
2018-02-09 15:02:33 +0100

    Update GLASSgo2CopraRNA_balanced_ooi_selection.r

--- GLASSgo2CopraRNA_balanced_ooi_selection.r
+++ GLASSgo2CopraRNA_balanced_ooi_selection.r
@@ -8,10 +8,10 @@ args <- commandArgs(trailingOnly = TRUE)
 
 ooi<-"NC_000913"
 wildcard<-c("NC_000913","NC_000911","NC_003197","NC_016810","NC_000964","NC_002516","NC_003210","NC_007795","NC_003047")
-max_number<-15
+max_number<-35
 outfile_prefix<-"sRNA"
 exclude<-c("NZ_CP009781.1","NZ_LN681227.1")
-sim<-3
+sim<-5
 
 for(i in 1:length(args)){
 	temp<-strsplit(args[i],"=")
